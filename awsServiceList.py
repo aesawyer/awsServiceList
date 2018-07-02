e1f76c4d28a57f0a5d6f2f0a85becbf25f0558d5
Adam Sawyer <aesawyer@live.com>
2018-07-02 09:24:14 -0600

    Began EC2 describe_instances implementation

--- awsServiceList.py
+++ awsServiceList.py
@@ -9,5 +9,15 @@ def lambda_handler(event,context):
     client.publish(
         TopicArn=(os.environ['TOPIC_ARN']),
         Subject=('AWS Servive List | ' + str(now.day) + ' ' + now.strftime("%B") + ', ' + str(now.year)),
-        Message="It worked"
+        Message=serv_list()
     )
+
+def serv_list():
+    ec2 = boto3.client('ec2')
+    s3 = boto3.client('s3')
+    lam = boto3.client('lambda')
+    dynamo = boto3.client('dynamodb')
+    rds = boto3.client('rds')
+
+    response = ec2.describe_instances()
+    return str(response)
